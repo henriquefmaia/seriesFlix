fe32572733916e874c7fbfbd241f6e88bda68cca
henriquefmaia <henriquefmaia@outlook.com>
2021-11-22 11:27:29 -0300

    Versão final - com categorias alteradas

--- DIO.series/enum/genero.cs
+++ DIO.series/enum/genero.cs
@@ -1,7 +1,19 @@
-namespace DIO.series.enum
+namespace DIO.Series
 {
-    public class genero
-    {
-        
-    }
+	public enum Genero
+	{
+		Acao = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Documentario = 4,
+		Drama = 5,
+		Thriller = 6,
+		Faroeste = 7,
+		Fantasia = 8,
+		SciFi = 9,
+		Musical = 10,
+		Romance = 11,
+		Suspense = 12,
+		Terror = 13
+	}
 }
